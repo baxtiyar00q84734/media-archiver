d4b8f6a286fafe5c7d0e0a160216d973efb0a3d8
BakhtiyarGuluzada <q.baxtiyar07@gmail.com>
2025-02-27 21:03:42 +0400

    front and back added

---
+++ media-archiver-backend/src/main/java/org/example/mediaarchiverbackend/MediaArchiverBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.mediaarchiverbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaArchiverBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaArchiverBackendApplication.class, args);
+    }
+
+}
---
+++ media-archiver-backend/src/test/java/org/example/mediaarchiverbackend/MediaArchiverBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mediaarchiverbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaArchiverBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
